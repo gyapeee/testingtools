7fe7e4e26020fd8fba8ab7c1488e12f04025204a
dkormos <david.kormos@goto.com>
2022-08-13 03:35:01 +0200

    2 worker thread and run 15 times

--- playwright/playwright.config.js
+++ playwright/playwright.config.js
@@ -4,9 +4,10 @@ const { devices } = require("@playwright/test");
 
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
+  repeatEach: 15,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 15, // Retry tests twice on CI, once locally
-  workers: process.env.CI ? 2 : undefined, // Limit the number of workers on CI, use default locally
+  retries: process.env.CI ? 2 : 1, // Retry tests twice on CI, once locally
+  workers: process.env.CI ? 2 : 2, // Limit the number of workers on CI, use default locally
   use: {
     headless: false,
     trace: "on-first-retry",
